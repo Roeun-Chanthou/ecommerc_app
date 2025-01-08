a98d62f321745a958d3e47cfd6c50b332388f782
Roeun-Chanthou <roeunchanthou1401@gmail.com>
2025-01-08 15:22:18 +0700

    update

--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -3,6 +3,7 @@ import 'package:ecommerc_app/pages/settings/setting.dart';
 import 'package:ecommerc_app/pages/wishlist/wish_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -24,157 +25,104 @@ class _MainScreenState extends State<MainScreen> {
           Setting(),
         ],
       ),
-      // bottomNavigationBar: Padding(
-      //   padding:
-      //       const EdgeInsets.only(bottom: 30, left: 20, right: 20, top: 10),
-      //   child: GNav(
-      //     onTabChange: (value) {
-      //       setState(() {
-      //         currentIndex = value;
-      //       });
-      //     },
-      //     tabBorderRadius: 15,
-      //     curve: Curves.easeIn,
-      //     duration: const Duration(milliseconds: 900),
-      //     gap: 6,
-      //     color: Colors.grey[800],
-      //     activeColor: Colors.black,
-      //     iconSize: 20,
-      //     tabBackgroundColor: Colors.purple.withOpacity(0.1),
-      //     padding: const EdgeInsets.symmetric(
-      //       horizontal: 20,
-      //       vertical: 5,
-      //     ),
-      //     tabs: [
-      //       GButton(
-      //         icon: Icons.home,
-      //         iconColor: Colors.white,
-      //         leading: currentIndex == 0
-      //             ? SvgPicture.asset(
-      //                 "assets/home_solid.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               )
-      //             : SvgPicture.asset(
-      //                 "assets/home 03.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               ),
-      //         text: 'Home',
-      //       ),
-      //       GButton(
-      //         icon: Icons.favorite,
-      //         leading: currentIndex == 1
-      //             ? SvgPicture.asset(
-      //                 "assets/love_solid.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               )
-      //             : SvgPicture.asset(
-      //                 "assets/love.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               ),
-      //         text: 'Wishlist',
-      //       ),
-      //       GButton(
-      //         icon: Icons.settings,
-      //         leading: currentIndex == 2
-      //             ? SvgPicture.asset(
-      //                 "assets/setting_solid.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               )
-      //             : SvgPicture.asset(
-      //                 "assets/setting.svg",
-      //                 colorFilter: const ColorFilter.mode(
-      //                   Colors.black,
-      //                   BlendMode.srcIn,
-      //                 ),
-      //               ),
-      //         text: 'Setting',
-      //       ),
-      //     ],
-      //   ),
-      // ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        onTap: (value) {
-          setState(
-            () {
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          bottom: 40,
+          left: 20,
+          right: 20,
+          top: 10,
+        ),
+        child: GNav(
+          onTabChange: (value) {
+            setState(() {
               currentIndex = value;
-            },
-          );
-        },
-        selectedItemColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(
-            icon: currentIndex == 0
-                ? SvgPicture.asset(
-                    "assets/home_solid.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
-                    ),
-                  )
-                : SvgPicture.asset(
-                    "assets/home 03.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-            label: 'Home',
+            });
+          },
+          tabBorderRadius: 15,
+          curve: Curves.easeIn,
+          duration: const Duration(milliseconds: 900),
+          gap: 6,
+          color: Colors.grey[800],
+          activeColor: Colors.black,
+          iconSize: 20,
+          tabBackgroundColor: Colors.purple.withOpacity(0.1),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 5,
           ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 1
-                ? SvgPicture.asset(
-                    "assets/love_solid.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              iconColor: Colors.white,
+              leading: currentIndex == 0
+                  ? SvgPicture.asset(
+                      "assets/home_solid.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/home 03.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
                     ),
-                  )
-                : SvgPicture.asset(
-                    "assets/love.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.favorite,
+              leading: currentIndex == 1
+                  ? SvgPicture.asset(
+                      "assets/love_solid.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/love.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
                     ),
-                  ),
-            label: 'Wishlist',
-          ),
-          BottomNavigationBarItem(
-            icon: currentIndex == 2
-                ? SvgPicture.asset(
-                    "assets/setting_solid.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
+              text: 'Wishlist',
+            ),
+            GButton(
+              icon: Icons.settings,
+              leading: currentIndex == 2
+                  ? SvgPicture.asset(
+                      "assets/setting_solid.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
+                    )
+                  : SvgPicture.asset(
+                      "assets/setting.svg",
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.srcIn,
+                      ),
                     ),
-                  )
-                : SvgPicture.asset(
-                    "assets/setting.svg",
-                    colorFilter: const ColorFilter.mode(
-                      Colors.black,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-            label: 'Setting',
-          ),
-        ],
+              text: 'Setting',
+            ),
+          ],
+        ),
       ),
     );
   }
